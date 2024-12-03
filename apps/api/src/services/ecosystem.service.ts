7ed2ef49b727b7d8f7f42e1b4150e149ed205f0b
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2024-12-02 22:20:26 -0500

    feat: update ecosystem url to main branch

--- apps/api/src/services/ecosystem.service.ts
+++ apps/api/src/services/ecosystem.service.ts
@@ -5,7 +5,7 @@ import { db, collection } from "database";
 
 export async function getEcosystems() {
   const response = await axios.get<{ ecosystems: Ecosystem[] }>(
-    "https://raw.githubusercontent.com/Passage-Chain/indexer/refs/heads/features/ecosystems/config/ecosystems.json"
+    "https://raw.githubusercontent.com/Passage-Chain/indexer/main/config/ecosystems.json"
   );
   const collections = await db.select().from(collection);
   const ecosystems = response.data.ecosystems.map((ecosystem) => ({
