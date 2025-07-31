0f1592a06ac2e9b4c1cbe1ce7ec6512f8c1b9e9d
Adrian Estevez <adrianeea.ae@gmail.com>
2025-07-31 15:11:51 -0400

    Add new token-related fields to collection schema

--- packages/database/drizzle/schema/collection.ts
+++ packages/database/drizzle/schema/collection.ts
@@ -30,6 +30,9 @@ export const collection = pgTable(
     royaltyFee: numeric("royalty_fee"),
     maxNumToken: integer("max_num_token"),
     perAddressLimit: integer("per_address_limit"),
+    airDroppedTokens: integer("airdropped_tokens").default(0),
+    mintableTokens: integer("mintable_tokens").default(0),
+    mintedTokens: integer("minted_tokens").default(0),
     whitelist: uuid("whitelist").references(() => whitelist.id, {
       onDelete: "cascade",
       onUpdate: "cascade",
